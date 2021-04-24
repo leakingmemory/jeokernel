63ebb7b22129a5bbba1b2107a8332355ab988591
Jan-Espen Oversand <sigsegv@radiotube.org>
2021-04-24 20:20:33 +0200

    Checkpoint: Although quirky, can compile and run with gcc cross compiler for x86_64.

--- boot32/toolchain.cmake
+++ boot32/toolchain.cmake
@@ -1,5 +1,5 @@
 
-set(USE_CLANG true)
+#set(USE_CLANG true)
 
 if(USE_CLANG)
     set(CMAKE_C_COMPILER /usr/bin/clang-11)
@@ -11,10 +11,11 @@ if(USE_CLANG)
 
     set(CMAKE_CXX_LINK_EXECUTABLE "clang++-11 -target i686-pc-none-elf -Wl,-no-pie -nostdinc -nostdlib -fuse-ld=lld -ffreestanding -T ${CMAKE_SOURCE_DIR}/boot32.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES> ")
 else(USE_CLANG)
+
     set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -m32 -march=i686 -nostdinc -nostdlib -ffreestanding -DIA32 -DLOADER ")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32 -march=i686 -nostdinc -nostdlib -ffreestanding -DIA32 -DLOADER ")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32 -march=i686 -nostdinc -nostdlib -ffreestanding -DIA32 -DLOADER ")
 
-    set(CMAKE_CXX_LINK_EXECUTABLE "g++ -Wl,--oformat=elf32-i686 -no-pie -nostdlib -fstack-protector -ffreestanding -T ${CMAKE_SOURCE_DIR}/boot32.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES> ")
+    set(CMAKE_CXX_LINK_EXECUTABLE "ld --oformat=elf32-i386 -melf_i386 -no-pie -nostdlib -T ${CMAKE_SOURCE_DIR}/boot32.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES> ")
 endif(USE_CLANG)
 
--- boot64/toolchain.cmake
+++ boot64/toolchain.cmake
@@ -1,5 +1,5 @@
 
-set(USE_CLANG true)
+#set(USE_CLANG true)
 
 if(USE_CLANG)
     set(CMAKE_C_COMPILER /usr/bin/clang-11)
@@ -11,6 +11,7 @@ if(USE_CLANG)
 
     set(CMAKE_CXX_LINK_EXECUTABLE "clang++-11 -target x86_64-pc-none-elf -Wl,-no-pie -nostdinc -nostdlib -fuse-ld=lld -ffreestanding -T ${CMAKE_SOURCE_DIR}/kernel.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES> -lgcc -lgcc_eh -lsupc++")
 else(USE_CLANG)
+
     set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -m64 -march=x86-64 -nostdinc -nostdlib -ffreestanding")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m64 -march=x86-64 -nostdinc -nostdlib -ffreestanding")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m64 -march=x86-64 -nostdinc -nostdlib -ffreestanding")
