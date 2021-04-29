93ea3172b20e27abbd8ad1ac7b20001f420ae4e4
Jan-Espen Oversand <sigsegv@radiotube.org>
2021-04-29 17:38:49 +0200

    Fix inappropriate assumptions with regards to build directory.

--- create_isoboot.sh
+++ create_isoboot.sh
@@ -4,7 +4,7 @@ if grub-file --is-x86-multiboot2  boot32/jeokernel; then
   mkdir -p mkdir -p iso/boot/grub
   cp -pv boot32/jeokernel iso/boot/loader
   cp -pv boot64/kernel iso/boot/kernel
-  cp -pv ../grub.cfg iso/boot/grub/
+  cp -pv ${CMAKE_SOURCE_DIR}/grub.cfg iso/boot/grub/
   grub-mkrescue -o jeokernel.iso iso/
 else
   echo "Not valid kernel"
