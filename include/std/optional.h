b1c938f540eba435c6017cb7e80d39fdf2995127
Jan-Espen Oversand <sigsegv@radiotube.org>
2021-05-07 00:59:52 +0200

    std::optional impl

---
+++ include/std/optional.h
@@ -0,0 +1,61 @@
+//
+// Created by sigsegv on 06.05.2021.
+//
+
+#ifndef JEOKERNEL_OPTIONAL_H
+#define JEOKERNEL_OPTIONAL_H
+
+#include <utility>
+
+namespace std {
+    template<class T>
+    class optional {
+    private:
+        uint8_t _data[sizeof(T)];
+        T *ptr;
+    public:
+        typedef T value_type;
+
+        constexpr optional()
+        noexcept : _data(), ptr(nullptr) {
+        }
+
+        template<class U = value_type>
+        constexpr optional(U &&value) {
+            ptr = new((void *) &(_data[0])) T(value);
+        }
+
+        constexpr optional( const optional& other ) : _data(), ptr(nullptr) {
+            if (other) {
+                ptr = new((void *) &(_data[0])) T(*other);
+            }
+        }
+
+        constexpr optional( optional&& other ) {
+            if (other) {
+                ptr = new((void *) &(_data[0])) T(std::move(*other));
+            }
+        }
+
+        ~optional() {
+            if (ptr != nullptr) {
+                ptr->~T();
+                ptr = nullptr;
+            }
+        }
+
+        T &operator*() const {
+            return *ptr;
+        }
+
+        constexpr T* operator->() {
+            return ptr;
+        }
+
+        operator bool() const noexcept {
+            return ptr != nullptr;
+        }
+    };
+}
+
+#endif //JEOKERNEL_OPTIONAL_H
