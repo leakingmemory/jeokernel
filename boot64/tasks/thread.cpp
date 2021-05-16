5c16b8b96a8a6d1cec1a1397b68baf14105f1ee5
Jan-Espen Oversand <sigsegv@radiotube.org>
2021-05-16 14:52:01 +0200

    Fix stack aligment to 16 byte for threads. Required for fpu/sse instructions.

--- boot64/tasks/thread.cpp
+++ boot64/tasks/thread.cpp
@@ -10,6 +10,8 @@
 
 
 extern "C" {
+    void thread_trampoline_start(std::threadimpl::thread_start_context *ctx);
+
     void thread_trampoline(std::threadimpl::thread_start_context *ctx) {
         ctx->invoke();
         uint8_t cpu_num{0};
@@ -29,7 +31,7 @@ namespace std {
         tasklist *scheduler = get_scheduler();
         {
             std::vector<task_resource *> resources{};
-            return scheduler->new_task((uint64_t) thread_trampoline, 0x8, (uint64_t) start_context, 0, 0, 0, 0, 0, resources);
+            return scheduler->new_task((uint64_t) thread_trampoline_start, 0x8, (uint64_t) start_context, 0, 0, 0, 0, 0, resources);
         }
     }
 
