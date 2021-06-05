60df0f9c0166f9735d72532b144070672df31f39
Jan-Espen Oversand <sigsegv@radiotube.org>
2021-06-05 11:58:56 +0200

    Get extended capabilities for xhci.

--- boot64/usb/xhci.cpp
+++ boot64/usb/xhci.cpp
@@ -18,10 +18,66 @@ Device *xhci_driver::probe(Bus &bus, DeviceInformation &deviceInformation) {
     }
 }
 
+#define DEBUG_XHCI_INIT_OWNER
+
 void xhci::init() {
+    uint64_t addr{0};
+    uint64_t size{0};
+    bool prefetch{false};
+    {
+        PciBaseAddressRegister bar0 = pciDeviceInformation.readBaseAddressRegister(0);
+        if (!bar0.is_memory()) {
+            get_klogger() << "USB xhci controller is invalid (invalid BAR0, not memory)\n";
+            return;
+        }
+        if (bar0.is_64bit()) {
+            addr = bar0.addr64();
+        } else if (bar0.is_32bit()) {
+            addr = (uint64_t) bar0.addr32();
+        } else {
+            get_klogger() << "USB xhci controller is invalid (invalid BAR0, invalid record)\n";
+            return;
+        }
+        size = bar0.memory_size();
+        if (size == 0) {
+            get_klogger() << "USB xhci controller is invalid (invalid BAR0, invalid size)\n";
+            return;
+        }
+        prefetch = bar0.is_prefetchable();
+    }
+#ifdef DEBUG_XHCI_INIT_OWNER
     {
         std::stringstream msg;
-        msg << DeviceType() << (unsigned int) DeviceId() << ": USB3 controller\n";
+        msg << DeviceType() << (unsigned int) DeviceId() << ": memory mapped registers at 0x" << std::hex << addr << " size 0x" << size << "\n";
+        get_klogger() << msg.str().c_str();
+    }
+#endif
+    uint64_t pg_offset{addr & 0xFFF};
+    mapped_registers_vm = std::make_unique<vmem>(size + pg_offset);
+    {
+        uint64_t physaddr{addr & 0xFFFFF000};
+        std::size_t pages = mapped_registers_vm->npages();
+        for (std::size_t page = 0; page < pages; page++) {
+            mapped_registers_vm->page(page).rwmap(physaddr, true, !prefetch);
+            physaddr += 0x1000;
+        }
+    }
+    capabilities = (xhci_capabilities *) (void *) (((uint8_t *) mapped_registers_vm->pointer()) + pg_offset);
+    opregs = capabilities->opregs();
+    {
+        std::optional<xhci_ext_cap> extcap = capabilities->extcap();
+        while (extcap)  {
+            {
+                std::stringstream msg;
+                msg << DeviceType() << (unsigned int) DeviceId() << ": ext cap type " << (unsigned int) extcap->cap_id << " value " << std::hex << extcap->value << "\n";
+                get_klogger() << msg.str().c_str();
+            }
+            extcap = extcap->next();
+        }
+    }
+    {
+        std::stringstream msg;
+        msg << DeviceType() << (unsigned int) DeviceId() << ": USB3 controller hccparams1=" << std::hex << capabilities->hccparams1 << "\n";
         get_klogger() << msg.str().c_str();
     }
 }
--- boot64/usb/xhci.h
+++ boot64/usb/xhci.h
@@ -6,11 +6,144 @@
 #define JEOKERNEL_XHCI_H
 
 #include <devices/drivers.h>
+#include <core/vmem.h>
 #include "../pci/pci.h"
+
+struct HostControllerPortRegisters {
+    uint32_t portStatusControl;
+    uint32_t portPowerManagementStatusControl;
+    uint32_t portLinkControl;
+    uint32_t portHardwareLPMControl;
+} __attribute__((__packed__));
+
+static_assert(sizeof(HostControllerPortRegisters) == 0x10);
+
+struct xhci_operational_registers {
+    uint32_t usbCommand;
+    uint32_t usbStatus;
+    uint32_t pageSize;
+    uint64_t reserved1;
+    uint32_t deviceNotificationControl;
+    uint64_t commandRingControl;
+    uint64_t reserved2;
+    uint64_t reserved3;
+    uint64_t deviceContextBaseAddressArrayPointer;
+    uint32_t config;
+    uint8_t reserved4[0x400-0x3C];
+    HostControllerPortRegisters portRegisters[1];
+} __attribute__((__packed__));
+
+static_assert(sizeof(xhci_operational_registers) == 0x410);
+
+struct xhci_ext_cap {
+    uint32_t *pointer;
+    union {
+        uint32_t value;
+        struct {
+            uint8_t cap_id;
+            uint8_t next_ptr;
+            uint16_t cap_specific;
+        } __attribute__((__packed__));
+    };
+
+    explicit xhci_ext_cap(uint32_t *pointer) : pointer(pointer), value(*pointer) {}
+    xhci_ext_cap(const xhci_ext_cap &cp) noexcept : pointer(cp.pointer), value(cp.value) {}
+    xhci_ext_cap(const xhci_ext_cap &&cp) noexcept : pointer(cp.pointer), value(cp.value) {}
+    xhci_ext_cap & operator = (const xhci_ext_cap &cp) noexcept {
+        this->pointer = cp.pointer;
+        this->value = cp.value;
+        return *this;
+    }
+    xhci_ext_cap & operator = (const xhci_ext_cap &&cp) noexcept {
+        this->pointer = cp.pointer;
+        this->value = cp.value;
+        return *this;
+    }
+
+    std::optional<xhci_ext_cap> next() {
+        if (next_ptr != 0) {
+            uint32_t *ptr = (uint32_t *) (void *) pointer;
+            uint16_t off = next_ptr;
+            ptr += off;
+            return { xhci_ext_cap(ptr) };
+        } else {
+            return { };
+        }
+    }
+};
+
+union xhci_hccparams1 {
+    uint32_t hccparams1;
+    struct {
+        bool ac64 : 1;
+        bool bnc : 1;
+        bool csz : 1;
+        bool ppc : 1;
+        bool pind : 1;
+        bool lhrC : 1;
+        bool ltc : 1;
+        bool nss : 1;
+        bool pae : 1;
+        bool spc : 1;
+        bool sec : 1;
+        bool cfc : 1;
+        uint8_t maxPSASize : 4;
+        uint16_t xhciExtCapPtr;
+    } __attribute__((__packed__));
+
+    explicit xhci_hccparams1(uint32_t hccparams1) noexcept : hccparams1(hccparams1) {}
+    xhci_hccparams1(const xhci_hccparams1 &cp) noexcept : hccparams1(cp.hccparams1) {}
+    xhci_hccparams1(const xhci_hccparams1 &&cp) noexcept : hccparams1(cp.hccparams1) {}
+    xhci_hccparams1 & operator = (const xhci_hccparams1 &cp) noexcept {
+        this->hccparams1 = cp.hccparams1;
+        return *this;
+    }
+    xhci_hccparams1 & operator = (const xhci_hccparams1 &&cp) noexcept {
+        this->hccparams1 = cp.hccparams1;
+        return *this;
+    }
+};
+
+struct xhci_capabilities {
+    uint8_t caplength;
+    uint8_t reserved;
+    uint16_t hciversion;
+    uint32_t hcsparams1;
+    uint32_t hcsparams2;
+    uint32_t hcsparams3;
+    uint32_t hccparams1;
+    uint32_t dboff;
+    uint32_t rtsoff;
+    uint32_t hccparams2;
+
+    xhci_operational_registers *opregs() {
+        uint8_t *ptr = (uint8_t *) (void *) this;
+        ptr += caplength;
+        ptr += 0x20;
+        return (xhci_operational_registers *) (void *) ptr;
+    }
+
+    std::optional<xhci_ext_cap> extcap() {
+        xhci_hccparams1 par(hccparams1);
+        if (par.xhciExtCapPtr != 0) {
+            uint32_t *ptr = (uint32_t *) (void *) this;
+            uint32_t off = par.xhciExtCapPtr;
+            ptr += off;
+            return { xhci_ext_cap(ptr) };
+        } else {
+            return { };
+        }
+    }
+} __attribute__((__packed__));
+
+static_assert(sizeof(xhci_capabilities) == 0x20);
 
 class xhci : public Device {
 private:
     PciDeviceInformation pciDeviceInformation;
+    std::unique_ptr<vmem> mapped_registers_vm;
+    xhci_capabilities *capabilities;
+    xhci_operational_registers *opregs;
 public:
     xhci(Bus &bus, PciDeviceInformation &deviceInformation) : Device("xhci", &bus), pciDeviceInformation(deviceInformation) {}
     void init() override;
