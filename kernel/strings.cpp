636c7fc2f5fdec688768d8be2007ccdb0f06d0b2
Jan-Espen Oversand <sigsegv@radiotube.org>
2021-12-26 16:54:24 +0100

    Faster memmove.

--- kernel/strings.cpp
+++ kernel/strings.cpp
@@ -5,6 +5,66 @@
 #include <strings.h>
 #include <string.h>
 
+template <class copy_type, class addr_type> void memmove_impl(void *dest, const void *src, size_t n) {
+    addr_type src_addr = (addr_type) src;
+    unsigned int src_alignment = sizeof(copy_type) - (src_addr % sizeof(copy_type));
+    if (src_alignment == sizeof(copy_type)) {
+        src_alignment = 0;
+    }
+    uint8_t *dest_ptr = (uint8_t *) dest;
+    const uint8_t *src_ptr = (uint8_t *) src;
+    const uint8_t *src_end = &(src_ptr[n]);
+    {
+        unsigned int i = 0;
+        while (i < src_alignment) {
+            *dest_ptr = *src_ptr;
+            ++dest_ptr;
+            ++src_ptr;
+            ++i;
+        }
+    }
+    addr_type dst_addr = (addr_type) dest_ptr;
+    unsigned int dst_alignment = sizeof(copy_type) - (dst_addr % sizeof(copy_type));
+    if (dst_alignment == sizeof(copy_type)) {
+        dst_alignment = 0;
+    }
+    {
+        unsigned int i = 0;
+        while (i < dst_alignment) {
+            *dest_ptr = src_ptr[i];
+            ++dest_ptr;
+            ++i;
+        }
+    }
+    copy_type *src_opt = (copy_type *) (void *) src_ptr;
+    copy_type *dst_opt = (copy_type *) (void *) dest_ptr;
+    copy_type *src_opt_end = (copy_type *) (((addr_type) src_end) - (((addr_type) src_end) % sizeof(copy_type)));
+    if (src_opt < src_opt_end) {
+        copy_type v1 = *src_opt;
+        ++src_opt;
+        unsigned int right_shift = dst_alignment << 3;
+        unsigned int left_shift = (sizeof(copy_type) << 3) - right_shift;
+        while (src_opt < src_opt_end) {
+            copy_type v2 = *src_opt;
+            ++src_opt;
+            copy_type dst_ins = right_shift == (sizeof(copy_type) << 3) ? 0 : v1 >> right_shift;
+            v1 = v2;
+            v2 = left_shift != (sizeof(copy_type) << 3) ? v2 << left_shift : 0;
+            dst_ins |= v2;
+            *dst_opt = dst_ins;
+            ++dst_opt;
+        }
+        dest_ptr = (uint8_t *) (void *) dst_opt;
+        dst_addr = (addr_type) dest;
+        src_ptr = ((uint8_t *) src) + (((addr_type) dest_ptr) - dst_addr);
+        while (src_ptr < src_end) {
+            *dest_ptr = *src_ptr;
+            ++src_ptr;
+            ++dest_ptr;
+        }
+    }
+}
+
 extern "C" {
 
     void *memmove(void *dest, const void *src, size_t n) {
@@ -13,10 +73,14 @@ extern "C" {
             const uint8_t *src_ptr = (uint8_t *) src;
             const uint8_t *src_end = &(src_ptr[n]);
             if (dest_ptr < src_ptr || dest_ptr > src_end) {
-                while (src_ptr < src_end) {
-                    *dest_ptr = *src_ptr;
-                    ++dest_ptr;
-                    ++src_ptr;
+                if (n > 32) {
+                    memmove_impl<uint64_t,uint64_t>(dest, src, n);
+                } else {
+                    while (src_ptr < src_end) {
+                        *dest_ptr = *src_ptr;
+                        ++dest_ptr;
+                        ++src_ptr;
+                    }
                 }
             } else {
                 uint8_t *dest_end = &(dest_ptr[n]);
---
+++ test/memmove.cpp
@@ -0,0 +1,26 @@
+//
+// Created by sigsegv on 12/26/21.
+//
+
+#include "tests.h"
+#include "../kernel/strings.cpp"
+
+int main() {
+    uint64_t comp[] = {0x1234567812345678, 0, 0x1234567812345678, 0, 0x1234567812345678, 0x1234567812345678};
+    uint64_t test1[] = {0x1234567812345678, 0, 0x1234567812345678, 0, 0x1234567812345678, 0x1234567812345678};
+    uint64_t test2[] = {0, 0, 0, 0, 0, 0};
+    memmove(test2, test1, 5 * 8);
+    assert(test2[0] == comp[0]);
+    assert(test2[1] == comp[1]);
+    assert(test2[2] == comp[2]);
+    assert(test2[3] == comp[3]);
+    assert(test2[4] == comp[4]);
+    memmove(test2, ((uint8_t *) (void *) test1) + 1, 5 * 8);
+    assert(test2[0] == 0x0012345678123456);
+    assert(test2[1] == 0x7800000000000000);
+    assert(test2[2] == 0x0012345678123456);
+    assert(test2[3] == 0x7800000000000000);
+    assert(test2[4] == 0x7812345678123456);
+    assert(test2[5] == 0);
+    return 0;
+}
