fcc0c2dc8b537a95cc8cf86fc8e4d69c5bc825cd
Jan-Espen Oversand <sigsegv@radiotube.org>
2022-06-15 20:54:24 +0200

    OOps: Missing file. Page fault handler can load userspace pages in and resume the process.

--- kernel/exec/process.cpp
+++ kernel/exec/process.cpp
@@ -8,6 +8,7 @@
 #include <strings.h>
 #include <mutex>
 #include <thread>
+#include <kfs/kfiles.h>
 #include "concurrency/raw_semaphore.h"
 
 PagetableRoot::PagetableRoot(uint16_t addr) : physpage(ppagealloc(PAGESIZE)), vm(PAGESIZE), branches((pagetable *) vm.pointer()), addr(addr) {
@@ -48,7 +49,7 @@ PagetableRoot::PagetableRoot(PagetableRoot &&mv) : physpage(mv.physpage), vm(std
 }
 
 MemMapping::MemMapping(std::shared_ptr<kfile> image, uint32_t pagenum, uint32_t pages, uint32_t image_skip_pages)
-: image(image), pagenum(pagenum), pages(pages), image_skip_pages(image_skip_pages) {}
+: image(image), pagenum(pagenum), pages(pages), image_skip_pages(image_skip_pages), mappings() {}
 
 Process::Process() : pagetableRoots(), mappings() {}
 
@@ -76,9 +77,9 @@ bool Process::Pageentry(uint32_t pagenum, std::function<void (pageentr &)> func)
         root = &(pagetableRoots.emplace_back(index));
     }
     {
-        constexpr uint32_t mask = 0x3FFFF;
+        constexpr uint32_t mask = 0x7FFFFFF;
         pageoff = pageoff & mask;
-        static_assert((mask >> 17) == 1);
+        static_assert((mask >> 18) == 0x1FF);
         index = pageoff >> 18;
     }
     auto &b1 = (*(root->branches))[index];
@@ -104,12 +105,13 @@ bool Process::Pageentry(uint32_t pagenum, std::function<void (pageentr &)> func)
         vm.reload();
     }
     {
-        constexpr uint32_t mask = 0x1FF;
+        constexpr uint32_t mask = 0x3FFFF;
         pageoff = pageoff & mask;
-        static_assert((mask >> 8) == 1);
+        static_assert((mask >> 9) == 0x1FF);
         index = pageoff >> 9;
     }
     auto &b2 = (*table)[index];
+    phys_t b2phys;
     if (b2.page_ppn == 0) {
         auto phys = ppagealloc(sizeof(pagetable));
         if (phys == 0) {
@@ -121,15 +123,20 @@ bool Process::Pageentry(uint32_t pagenum, std::function<void (pageentr &)> func)
         b2.user_access = 1;
         b2.page_ppn = (phys >> 12);
         b2.execution_disabled = 0;
-        vm.page(0).rwmap((b2.page_ppn << 12));
+        b2phys = b2.page_ppn << 12;
+        vm.page(0).rwmap(b2phys);
         vm.reload();
         bzero(table, sizeof(*table));
     } else {
-        vm.page(0).rwmap((b2.page_ppn << 12));
+        b2phys = b2.page_ppn << 12;
+        vm.page(0).rwmap(b2phys);
         vm.reload();
     }
-    auto &b3 = (*table)[pageoff];
+    auto &b3 = (*table)[pageoff & 0x1FF];
     func(b3);
+    std::cout << std::hex << (b2phys + (pageoff & 0x1FF)) << " " << (b3.present != 0 ? "p" : "*") << (b3.user_access != 0 ? "u" : "k") << (b3.writeable != 0 ? "w" : "r")
+              << (b3.execution_disabled != 0 ? "n" : "e") << " " << b3.page_ppn << std::dec
+              << "\n";
     return true;
 }
 
@@ -184,7 +191,8 @@ void Process::task_enter() {
     critical_section cli{};
     auto &pt = get_root_pagetable();
     for (auto &root : pagetableRoots) {
-        auto &pe = pt[root.addr + PMLT4_USERSPACE_START];
+        auto pe_index = root.addr + PMLT4_USERSPACE_START;
+        auto &pe = pt[pe_index];
         pe.present = 1;
         pe.writeable = 1;
         pe.user_access = 1;
@@ -200,7 +208,7 @@ void Process::task_enter() {
         pe.reserved1 = 0;
         pe.os_virt_start = 0;
         pe.ignored3 = 0;
-        pe.execution_disabled = false;
+        pe.execution_disabled = 0;
     }
     reload_pagetables();
 }
@@ -221,7 +229,8 @@ static std::vector<process_pfault> faults{};
 static std::thread *pfault_thread{nullptr};
 
 bool Process::page_fault(task &current_task, Interrupt &intr) {
-    std::cout << "Page fault in user process\n";
+    std::cout << "Page fault in user process with pagetable root " << std::hex << ((uintptr_t) get_root_pagetable())
+    << " code " << intr.error_code() << std::dec << "\n";
     current_task.set_blocked(true);
     uint64_t address{0};
     asm("mov %%cr2, %0" : "=r"(address));
@@ -253,6 +262,121 @@ bool Process::page_fault(task &current_task, Interrupt &intr) {
 
 void Process::resolve_page_fault(task &current_task, uintptr_t ip, uintptr_t fault_addr) {
     auto *scheduler = get_scheduler();
+    std::unique_ptr<std::lock_guard<std::mutex>> lock{new std::lock_guard(mtx)};
+    {
+        constexpr uint64_t relocationOffset = ((uint64_t) PMLT4_USERSPACE_START) << (9 + 9 + 9 + 12);
+        if (fault_addr < relocationOffset) {
+            goto pfault_fail;
+        }
+        uint32_t page;
+        {
+            uintptr_t addr{fault_addr - relocationOffset};
+            addr = addr >> 12;
+            page = addr;
+        }
+        MemMapping *mapping{nullptr};
+        for (auto &m : mappings) {
+            if (m.pagenum <= page && page < (m.pagenum + m.pages)) {
+                mapping = &m;
+                break;
+            }
+        }
+        if (mapping == nullptr) {
+            goto pfault_fail;
+        }
+        phys_t physpage = mapping->image_skip_pages + (page - mapping->pagenum);
+        uint16_t rootAddr;
+        {
+            uint32_t addr{page};
+            addr = addr >> (9+9+9);
+            rootAddr = addr;
+        }
+        PagetableRoot *root{nullptr};
+        for (auto &r : pagetableRoots) {
+            if (r.addr == rootAddr) {
+                root = &r;
+                break;
+            }
+        }
+        if (root == nullptr) {
+            goto pfault_fail;
+        }
+        auto b1 = (*(root->branches))[(page >> (9+9)) & 511];
+        if (b1.present == 0) {
+            goto pfault_fail;
+        }
+        vmem vm{PAGESIZE};
+        vm.page(0).rmap(b1.page_ppn << 12);
+        vm.reload();
+        auto b2 = (*((pagetable *) vm.pointer()))[(page >> 9) & 511];
+        if (b2.present == 0) {
+            goto pfault_fail;
+        }
+        vm.page(0).rwmap(b2.page_ppn << 12);
+        vm.reload();
+        auto b3 = (*((pagetable *) vm.pointer()))[page & 511];
+        if (b3.present == 0) {
+            std::cout << "Loading page " << std::hex << page << " for addr " << fault_addr << " from " << physpage << std::dec << "\n";
+            lock = {};
+            auto image = mapping->image;
+            auto loaded_page = image->GetPage(physpage);
+            lock = std::make_unique<std::lock_guard<std::mutex>>(mtx);
+            bool ok{false};
+            for (auto &m : mappings) {
+                if (m.pagenum <= page && page < (m.pagenum + m.pages)) {
+                    if (mapping != &m || physpage != (m.image_skip_pages + (page - m.pagenum)) || image != m.image) {
+                        goto pfault_fail;
+                    }
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                goto pfault_fail;
+            }
+            ok = false;
+            for (auto &r : pagetableRoots) {
+                if (r.addr == rootAddr) {
+                    if (root != &r) {
+                        goto pfault_fail;
+                    }
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                goto pfault_fail;
+            }
+            auto &b1 = (*(root->branches))[(page >> (9+9)) & 511];
+            std::cout << "Root branch " << std::hex << ((root->physpage << 12) + (((page >> (9+9)) & 511) * sizeof(b1))) << std::dec << "\n";
+            if (b1.present == 0) {
+                goto pfault_fail;
+            }
+            vm.page(0).rmap(b1.page_ppn << 12);
+            vm.reload();
+            auto &b2 = (*((pagetable *) vm.pointer()))[(page >> 9) & 511];
+            if (b2.present == 0) {
+                goto pfault_fail;
+            }
+            auto b2phys = b2.page_ppn << 12;
+            vm.page(0).rwmap(b2phys);
+            vm.reload();
+            auto &b3 = (*((pagetable *) vm.pointer()))[page & 511];
+            if (b3.present == 0) {
+                std::cout << std::hex << ((b2phys) + (page & 511)) << " *" << (b3.user_access != 0 ? "u" : "k") << (b3.writeable != 0 ? "w" : "r")
+                << (b3.execution_disabled != 0 ? "n" : "e") << " " << b3.page_ppn << std::dec
+                << "\n";
+                mapping->mappings.push_back({.data = loaded_page, .page = page});
+                b3.page_ppn = loaded_page.PhysAddr() >> 12;
+                b3.present = 1;
+                reload_pagetables();
+            }
+            current_task.set_blocked(false);
+            return;
+        } else {
+        }
+    }
+pfault_fail:
     std::cerr << "PID " << current_task.get_id() << ": Page fault at " << std::hex << ip << " addr " << fault_addr << std::dec << "\n";
     scheduler->terminate_blocked(&current_task);
 }
