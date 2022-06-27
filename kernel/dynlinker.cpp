02a78624b9a3ca067ddb3682937aa51b46521bad
Jan-Espen Oversand <sigsegv@radiotube.org>
2022-06-27 22:23:03 +0200

    Clang must have the __dso_handle symbol.

--- kernel/dynlinker.cpp
+++ kernel/dynlinker.cpp
@@ -6,15 +6,14 @@
 
 #define NUM_DESTRUCTORS 128
 
+void *__dso_handle = nullptr;
+
 static void (*dest_func[NUM_DESTRUCTORS]) (void *);
 static void *dest_arg[NUM_DESTRUCTORS];
 static int numDestructors = 0;
 
 extern "C" {
     int __cxa_atexit(void (*func)(void *), void *arg, void *dso_handle) {
-        if (dso_handle != nullptr) {
-            wild_panic("Dynamic linking is not yet supported");
-        }
         if (numDestructors < NUM_DESTRUCTORS) {
             dest_func[numDestructors] = func;
             dest_arg[numDestructors] = arg;
