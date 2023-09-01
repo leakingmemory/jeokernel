2b43fe02579c6d1b7cf2ed59b5dfad2b40c9c164
Jan-Espen Oversand <sigsegv@radiotube.org>
2023-09-01 07:19:02 +0200

    Check permissions for kill.

--- kernel/syscall/Kill.cpp
+++ kernel/syscall/Kill.cpp
@@ -7,23 +7,32 @@
 #include <errno.h>
 
 int64_t Kill::Call(int64_t ipid, int64_t isig, int64_t, int64_t, SyscallAdditionalParams &) {
+    auto *scheduler = get_scheduler();
+    uid_t euid;
+    pid_t our_pid;
+    {
+        auto &task = scheduler->get_current_task();
+        auto *pt = task.get_resource<ProcThread>();
+        euid = pt->geteuid();
+        our_pid = pt->getpid();
+    }
     std::shared_ptr<Process> process{};
     auto sig = (int) isig;
-    {
-        auto pid = (pid_t) ipid;
-        if (pid <= 0) {
-            return -EOPNOTSUPP;
-        }
-        auto *scheduler = get_scheduler();
-        scheduler->all_tasks([pid, &process](task &t) {
-            auto *pt = t.get_resource<ProcThread>();
-            if (pt != nullptr && pt->getpid() == pid) {
-                process = pt->GetProcess();
-            }
-        });
+    auto pid = (pid_t) ipid;
+    if (pid <= 0) {
+        return -EOPNOTSUPP;
     }
+    scheduler->all_tasks([pid, &process](task &t) {
+        auto *pt = t.get_resource<ProcThread>();
+        if (pt != nullptr && pt->getpid() == pid) {
+            process = pt->GetProcess();
+        }
+    });
     if (!process) {
         return -ESRCH;
+    }
+    if (our_pid != pid && euid != 0 && euid != process->getuid()) {
+        return -EPERM;
     }
     int err = process->setsignal(sig);
     if (err > 0) {
