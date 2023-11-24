6b6ec31c6bfa96dcb44226072450f8e948adbb27
Jan-Espen Oversand <sigsegv@radiotube.org>
2023-11-24 21:03:29 +0100

    Test multiple lock calls.

--- test/weakptr.cpp
+++ test/weakptr.cpp
@@ -49,5 +49,25 @@ int main() {
         std::shared_ptr<testclass> cp = weak1.lock();
         assert(!cp);
     }
+    int second{0};
+    {
+        std::weak_ptr<testclass> weak1{};
+        {
+            std::shared_ptr<testclass> shared1 = std::make_shared<testclass>(second);
+            {
+                std::weak_ptr<testclass> weak{shared1};
+                weak1 = weak;
+            }
+            std::shared_ptr<testclass> copy1 = weak1.lock();
+            assert(copy1);
+            std::shared_ptr<testclass> copy2 = weak1.lock();
+            assert(copy2);
+            std::shared_ptr<testclass> copy3 = weak1.lock();
+            assert(copy3);
+        }
+        assert(first == 1);
+        std::shared_ptr<testclass> cp = weak1.lock();
+        assert(!cp);
+    }
     return 0;
 }
