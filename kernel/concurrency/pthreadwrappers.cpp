3045ada098f7f1507411e7446aa99162b6e63b2a
Jan-Espen Oversand <sigsegv@radiotube.org>
2026-04-07 20:44:16 +0200

    Fix a few issues compiling right now. Stubs for pthread funcs, should ideally be implemented fully.

--- kernel/concurrency/pthreadwrappers.cpp
+++ kernel/concurrency/pthreadwrappers.cpp
@@ -6,6 +6,19 @@
 
 extern "C" {
 
+    int pthread_once(pthread_once_t *once_control, void (*init_routine)(void)) {
+        // TODO
+    }
+    int pthread_key_create(pthread_key_t *key, void (*destructor)(void *)) {
+        // TODO
+    }
+    int pthread_setspecific(pthread_key_t key, const void *value) {
+        // TODO
+    }
+    void *pthread_getspecific(pthread_key_t key) {
+        // TODO
+    }
+
     int pthread_mutex_lock(pthread_mutex_t *mutex) {
         mutex->mtx.lock();
         return 0;
--- kernel/toolchain.cmake
+++ kernel/toolchain.cmake
@@ -14,7 +14,7 @@ if(USE_CLANG)
     IF(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
         set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> -nostdlib --pic-executable -T ${CMAKE_SOURCE_DIR}/kernel.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES>")
     ELSE()
-        set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_COMPILER} -target x86_64-pc-none-elf¨ -mno-red-zone -nostdinc -fPIC -nostdlib -fuse-ld=lld -ffreestanding -fno-stack-protector -T ${CMAKE_SOURCE_DIR}/kernel.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES> -lgcc")
+        set(CMAKE_CXX_LINK_EXECUTABLE "${CMAKE_CXX_COMPILER} -target x86_64-pc-none-elf -mno-red-zone -nostdinc -fPIC -nostdlib -fuse-ld=lld -ffreestanding -fno-stack-protector -T ${CMAKE_SOURCE_DIR}/kernel.ld <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES> -lgcc")
     ENDIF()
 else(USE_CLANG)
 
