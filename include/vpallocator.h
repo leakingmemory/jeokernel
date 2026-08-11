d652b936e0f5fc9097e87b5c0efb4257adbb0a57
Jan-Espen Oversand <sigsegv@radiotube.org>
2026-08-11 15:44:02 +0200

    Self expanding vpalloc algorithm.

--- include/vpallocator.h
+++ include/vpallocator.h
@@ -20,9 +20,23 @@ struct VPAllocatorData {
 	constexpr VPAllocatorData() = default;
 };
 
+struct VPAllocatorPage;
+
 struct VPAllocatorLink {
-	phys_t paddr{std::numeric_limits<phys_t>::max()};
-	uintptr_t vaddr{std::numeric_limits<uintptr_t>::max()};
+	union {
+		phys_t addr;
+		VPAllocatorPage *ptr;
+	} paddr;
+	union {
+		uintptr_t addr;
+		VPAllocatorPage *ptr;
+	} vaddr;
+
+	constexpr VPAllocatorLink() noexcept {
+		paddr.addr = 0;
+		paddr.ptr = nullptr;
+		vaddr.ptr = nullptr;
+	}
 };
 
 enum class VPAllocatorResult {
@@ -39,14 +53,17 @@ struct VPAllocatorPage {
 
 	constexpr VPAllocatorPage() = default;
 
-	constexpr std::optional<uintptr_t> GetStart() {
+	constexpr bool HasFree() const {
+		return data.num > 0;
+	}
+	constexpr std::optional<uintptr_t> GetStart() const {
 		if (data.num > 0) {
 			return { ranges[0].start };
 		} else {
 			return {};
 		}
 	}
-	constexpr std::optional<uintptr_t> GetEnd() {
+	constexpr std::optional<uintptr_t> GetEnd() const {
 		if (data.num > 0) {
 			return { ranges[data.num - 1].end };
 		} else {
@@ -194,7 +211,7 @@ struct VPAllocatorPage {
 				} else if ((addr + size) < ranges[i + 1].start) {
 					if (data.num < num_records) {
 						for (decltype(data.num) j = 0; j < (data.num - i - 1); j++) {
-							ranges[data.num - j + i] = ranges[data.num - j + i - 1];
+							ranges[data.num - j] = ranges[data.num - j - 1];
 						}
 						++data.num;
 						ranges[i + 1].start = addr;
@@ -210,14 +227,23 @@ struct VPAllocatorPage {
 		}
 		return VPAllocatorResult::ERROR;
 	}
-	VPAllocatorPage *GetNext() {
-		if (next.vaddr != std::numeric_limits<decltype(next.vaddr)>::max()) {
-			return reinterpret_cast<VPAllocatorPage *>(next.vaddr);
-		} else if (next.paddr != std::numeric_limits<decltype(next.paddr)>::max()) {
-			return reinterpret_cast<VPAllocatorPage *>(static_cast<uintptr_t>(next.paddr));
+	constexpr VPAllocatorPage *GetNext() {
+		if (next.vaddr.ptr != nullptr) {
+			return next.vaddr.ptr;
+		} else if (next.paddr.ptr != nullptr) {
+			return next.paddr.ptr;
 		} else {
 			return nullptr;
 		}
+	}
+	constexpr void SetNextVirtual(VPAllocatorPage *n) {
+		next.vaddr.ptr = n;
+		next.paddr.addr = 0;
+	}
+	constexpr void SetNextPhysical(VPAllocatorPage *n) {
+		next.vaddr.ptr = nullptr;
+		next.paddr.addr = 0;
+		next.paddr.ptr = n;
 	}
 };
 
@@ -249,5 +275,328 @@ constexpr bool VPAllocatorTest1() {
 	return true;
 }
 static_assert(VPAllocatorTest1());
+
+template <class T> concept VPAllocatorPageAllocator = requires(T allocator) {
+	{ allocator.TryAllocate() } -> std::same_as<std::optional<VPAllocatorPage *>>;
+	{ allocator.Free(std::declval<VPAllocatorPage *>()) };
+	{ allocator.IsVirtualAddress() } -> std::same_as<bool>;
+};
+
+template <VPAllocatorPageAllocator PageAllocator> class VPAllocator {
+private:
+	PageAllocator *pageAllocator;
+	VPAllocatorPage *first{nullptr};
+public:
+	constexpr VPAllocator(PageAllocator *pageAllocator, VPAllocatorPage *root) : pageAllocator(pageAllocator), first(root) {
+	}
+	constexpr ~VPAllocator() = default;
+
+	constexpr void MoveOneUp(VPAllocatorPage *page, VPAllocatorPage *next) {
+		if (next->data.num >= next->num_records || page->data.num <= 0) {
+			return;
+		}
+		next->ranges[next->data.num++] = page->ranges[--page->data.num];
+	}
+	constexpr void MoveOneUp(VPAllocatorPage *page) {
+		auto *next = page->GetNext();
+		if (next != nullptr) {
+			MoveOneUp(page, next);
+		}
+	}
+	constexpr VPAllocatorPage *MoreRoom(VPAllocatorPage *page, VPAllocatorPage *next) {
+		if (page == nullptr) {
+			return next;
+		}
+		if (next == nullptr || (next->num_records - next->data.num) < 2) {
+			{
+				std::optional<VPAllocatorPage *> perhapsNewpage = pageAllocator->TryAllocate();
+				if (!perhapsNewpage) {
+					return next;
+				}
+				if (next != nullptr) {
+					(*perhapsNewpage)->next = page->next;
+				}
+				next = *perhapsNewpage;
+			}
+			if (pageAllocator->IsVirtualAddress()) {
+				page->SetNextVirtual(next);
+			} else {
+				page->SetNextPhysical(next);
+			}
+			auto half_num = page->data.num / 2;
+			if (half_num <= 0 && page->data.num > 0) {
+				half_num = 1;
+			}
+			for (decltype(page->data.num) i = 0; i < half_num; i++) {
+				MoveOneUp(page, next);
+			}
+			return next;
+		}
+		MoveOneUp(page, next);
+		MoveOneUp(page, next);
+		return next;
+	}
+	constexpr VPAllocatorResult TryAllocateFromAddress(uintptr_t addr, uintptr_t size) {
+		if (pageAllocator == nullptr) {
+			return VPAllocatorResult::ERROR;
+		}
+		auto *allocator = first;
+		while (allocator != nullptr) {
+			auto *next = allocator->GetNext();
+			if (allocator->HasFree()) {
+				if (allocator->GetStart() > addr) {
+					return VPAllocatorResult::ERROR;
+				} else if (allocator->GetEnd() < addr) {
+					allocator = next;
+					continue;
+				} else {
+					auto result = allocator->TryAllocateFromAddress(addr, size);
+					if (result == VPAllocatorResult::NO_SPACE) {
+						next = MoreRoom(allocator, next);
+						result = allocator->TryAllocateFromAddress(addr, size);
+					}
+					if (result == VPAllocatorResult::DONE || result == VPAllocatorResult::NO_SPACE) {
+						return result;
+					}
+				}
+			}
+			allocator = next;
+		}
+		return VPAllocatorResult::ERROR;
+	}
+	constexpr std::optional<uintptr_t> TryAllocate(uintptr_t size) {
+		if (pageAllocator == nullptr) {
+			return {};
+		}
+		auto *allocator = first;
+		while (allocator != nullptr) {
+			if (allocator->HasFree()) {
+				auto result = allocator->TryAllocate(size);
+				if (result) {
+					return result;
+				}
+			}
+			allocator = allocator->GetNext();
+		}
+		return {};
+	}
+	constexpr VPAllocatorResult TryFree(uintptr_t addr, uintptr_t size) {
+		if (pageAllocator == nullptr) {
+			return VPAllocatorResult::ERROR;
+		}
+		auto *allocator = first;
+		decltype(allocator) theEndingPage = nullptr;
+		uintptr_t end{};
+		bool allEmpty{true};
+		while (allocator != nullptr) {
+			auto *next = allocator->GetNext();
+			if (allocator->HasFree() || !allEmpty) {
+				allEmpty = false;
+				if (allocator->HasFree()) {
+					theEndingPage = allocator;
+					end = allocator->GetEnd();
+				}
+				if (end < addr && next != nullptr) {
+					allocator = next;
+					continue;
+				} else {
+					auto result = allocator->TryFree(addr, size);
+					if (result == VPAllocatorResult::NO_SPACE) {
+						next = MoreRoom(allocator, next);
+						result = allocator->TryFree(addr, size);
+					}
+					if (result == VPAllocatorResult::DONE || result == VPAllocatorResult::NO_SPACE) {
+						return result;
+					}
+				}
+			}
+			allocator = next;
+		}
+		if (allEmpty && first != nullptr) {
+			return first->TryFree(addr, size);
+		}
+		if (theEndingPage != nullptr) {
+			auto result = theEndingPage->TryFree(addr, size);
+			if (result == VPAllocatorResult::NO_SPACE) {
+				MoreRoom(theEndingPage, theEndingPage->GetNext());
+				return TryFree(addr, size);
+			}
+			return result;
+		}
+		return VPAllocatorResult::ERROR;
+	}
+};
+
+constexpr bool VPAllocatorTest2() {
+	VPAllocatorPage pg{};
+	size_t size{0x10000000};
+	size_t off{0};
+	while (size > 0) {
+		if (pg.TryAllocate(size)) {
+			return false;
+		}
+		{
+			auto free_res = pg.TryFree(off, size);
+			if (free_res == VPAllocatorResult::NO_SPACE) {
+				return true;
+			}
+			if (free_res != VPAllocatorResult::DONE) {
+				return false;
+			}
+		}
+		{
+			auto allocres = pg.TryAllocate(size);
+			if (!allocres || *allocres != off) {
+				return false;
+			}
+		}
+		if (pg.TryAllocate(size)) {
+			return false;
+		}
+		{
+			auto free_res = pg.TryFree(off, size);
+			if (free_res == VPAllocatorResult::NO_SPACE) {
+				return true;
+			}
+			if (free_res != VPAllocatorResult::DONE) {
+				return false;
+			}
+		}
+		size -= 0x2000;
+		{
+			auto allocres = pg.TryAllocate(0x2000);
+			if (!allocres || *allocres != off) {
+				return false;
+			}
+		}
+		{
+			off += 0x2000;
+			auto allocres = pg.TryAllocate(size);
+			if (!allocres || *allocres != off) {
+				return false;
+			}
+			{
+				auto free_res = pg.TryFree(off - 0x2000, 0x1000);
+				if (free_res == VPAllocatorResult::NO_SPACE) {
+					return true;
+				}
+				if (free_res != VPAllocatorResult::DONE) {
+					return false;
+				}
+			}
+			{
+				auto free_res = pg.TryFree(off + size - 0x1000, 0x1000);
+				if (free_res == VPAllocatorResult::NO_SPACE) {
+					return true;
+				}
+				if (free_res != VPAllocatorResult::DONE) {
+					return false;
+				}
+			}
+		}
+		size -= 0x2000;
+	}
+	return true;
+}
+static_assert(VPAllocatorTest2());
+
+class VPTestPageAllocator {
+private:
+	VPAllocatorPage pages[64] = {};
+	bool pagesInUse[64] = {};
+public:
+	constexpr VPTestPageAllocator() = default;
+	constexpr ~VPTestPageAllocator() noexcept = default;
+	constexpr std::optional<VPAllocatorPage *> TryAllocate() {
+		for (size_t i = 0; i < 64; i++) {
+			if (!pagesInUse[i]) {
+				pagesInUse[i] = true;
+				return &(pages[i]);
+			}
+		}
+		return {};
+	}
+	constexpr void Free(VPAllocatorPage *p) {
+	}
+	constexpr bool IsVirtualAddress() {
+		return true;
+	}
+};
+
+constexpr bool VPAllocatorTest3() {
+	VPTestPageAllocator testPageAllocator{};
+	VPAllocatorPage root{};
+	VPAllocator<VPTestPageAllocator> pg{&testPageAllocator, &root};
+	size_t size{0x10000000};
+	size_t off{0};
+	for (size_t n = 0; n < 64; n++) {
+		if (pg.TryAllocate(size)) {
+			return false;
+		}
+		{
+			auto free_res = pg.TryFree(off, size);
+			if (free_res == VPAllocatorResult::NO_SPACE) {
+				return true;
+			}
+			if (free_res != VPAllocatorResult::DONE) {
+				return false;
+			}
+		}
+		{
+			auto allocres = pg.TryAllocate(size);
+			if (!allocres || *allocres != off) {
+				return false;
+			}
+		}
+		if (pg.TryAllocate(size)) {
+			return false;
+		}
+		{
+			auto free_res = pg.TryFree(off, size);
+			if (free_res == VPAllocatorResult::NO_SPACE) {
+				return true;
+			}
+			if (free_res != VPAllocatorResult::DONE) {
+				return false;
+			}
+		}
+		size -= 0x2000;
+		{
+			auto allocres = pg.TryAllocate(0x2000);
+			if (!allocres || *allocres != off) {
+				return false;
+			}
+		}
+		{
+			off += 0x2000;
+			auto allocres = pg.TryAllocate(size);
+			if (!allocres || *allocres != off) {
+				return false;
+			}
+			{
+				auto free_res = pg.TryFree(off - 0x2000, 0x1000);
+				if (free_res == VPAllocatorResult::NO_SPACE) {
+					return true;
+				}
+				if (free_res != VPAllocatorResult::DONE) {
+					return false;
+				}
+			}
+			{
+				auto free_res = pg.TryFree(off + size - 0x1000, 0x1000);
+				if (free_res == VPAllocatorResult::NO_SPACE) {
+					return true;
+				}
+				if (free_res != VPAllocatorResult::DONE) {
+					return false;
+				}
+			}
+		}
+		size -= 0x2000;
+	}
+	return true;
+}
+
+static_assert(VPAllocatorTest3());
 
 #endif
